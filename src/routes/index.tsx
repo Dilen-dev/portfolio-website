e7989ef669511e4f67295920923b504a407a533d
Lenyolosa <lenyolosa@outlook.com>
2026-08-09 04:51:14 +0200

    added changing text animations

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,5 @@
 import { createFileRoute } from '@tanstack/react-router'
+import Typewriter from 'typewriter-effect'
 
 export const Route = createFileRoute('/')({ component: App })
 
@@ -7,7 +8,15 @@ function App() {
     <main className="page-wrap px-4 pb-8 pt-14">
       <div className="row-container">
         <section className="hero">
-          <h1 style={{ fontSize: '4rem' }}>Shaping ideas</h1>
+          <h1 style={{ fontSize: '4rem', display: 'inline-flex' }}>Shaping&nbsp;{''}
+            <Typewriter
+              options={{
+                strings: ['ideas','concepts','designs','pipelines','code'],
+                autoStart: true,
+                loop: true,
+              }}
+            />
+          </h1>
           <h1 style={{ fontSize: '4rem' }}>into Real Projects</h1>
           <h1 style={{ fontSize: '4rem' }}>that Deliver Results</h1>
           <h6 style={{ fontSize: '0.9rem', fontFamily: 'Literata, serif',fontOpticalSizing: 'auto',fontWeight: 400 }}> Hi, I'm Lenyolosa, a developer based in Lesotho with a passion for solving problems through code and data.</h6>
@@ -28,6 +37,11 @@ function App() {
             <h2 style={{ fontSize: '1.7rem' }}> Analytics Engineering</h2>
             <h2 style={{ fontSize: '1.7rem' }}> Database Management</h2>
           </div>
+        </article>
+      </div>
+      <div>
+        <article className='skills'>
+          <h1 style={{ fontSize: '2rem '}}>My Skills</h1>
         </article>
       </div>
     </main>
